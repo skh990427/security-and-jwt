42cf64a5faed097ba553504391104e29a54ee3df
KwangHo <skh990427@naver.com>
2024-04-22 23:56:57 +0900

    feat: 로그인 - 로그인 필터 구현

---
+++ src/main/java/com/example/studyjwt/jwt/LoginFilter.java
@@ -0,0 +1,35 @@
+package com.example.studyjwt.jwt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    public LoginFilter(AuthenticationManager authenticationManager, AuthenticationManager authenticationManager1) {
+        super(authenticationManager);
+        this.authenticationManager = authenticationManager1;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        return authenticationManager.authenticate(authToken);
+    }
+
+
+}
