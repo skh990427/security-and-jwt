5f25abbe41f78a2d9e0d6e094353ed8698f82de7
KwangHo <skh990427@naver.com>
2024-04-23 22:01:29 +0900

    feat: JWT 발급 및 검증 클래스 JwtUtil

---
+++ src/main/java/com/example/studyjwt/jwt/JwtUtil.java
@@ -0,0 +1,44 @@
+package com.example.studyjwt.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private SecretKey secretKey;
+
+    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+        this.secretKey
+                = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredMs) {
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis())) //현재 발행 시간
+                .expiration(new Date(System.currentTimeMillis() + expiredMs)) //토큰 만료 시간
+                .signWith(secretKey) //토큰 시그니처를 만들어서 암호화
+                .compact();
+    }
+}
