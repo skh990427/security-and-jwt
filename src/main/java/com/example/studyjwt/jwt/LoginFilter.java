4dd6531c0496c55b57417a2c710b7874c4d5d870
KwangHo <skh990427@naver.com>
2024-04-23 20:06:04 +0900

    feat: 로그인 - 로그인 필터 수정

--- src/main/java/com/example/studyjwt/jwt/LoginFilter.java
+++ src/main/java/com/example/studyjwt/jwt/LoginFilter.java
@@ -1,5 +1,7 @@
 package com.example.studyjwt.jwt;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,6 +9,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -16,20 +20,26 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         this.authenticationManager = authenticationManager;
     }
 
-    public LoginFilter(AuthenticationManager authenticationManager, AuthenticationManager authenticationManager1) {
-        super(authenticationManager);
-        this.authenticationManager = authenticationManager1;
-    }
-
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         String username = obtainUsername(request);
         String password = obtainPassword(request);
 
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+        //인증 매니저한테 어떤 dto에 담아서 넘겨줘야하는데 그 dto가 UsernamePasswordAuthenticationToken 임
 
         return authenticationManager.authenticate(authToken);
     }
 
+    //로그인 성공시 실행하는 메서드
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
 
+    }
+
+    //로그인 실패시 실행하는 메서드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+
+    }
 }
